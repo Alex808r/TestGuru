1e25e8ed05b336827b82bb9b01fd137df1924975
Alex808r <fromxlam@gmail.com>
2021-09-26 01:07:25 +0300

    Change render file ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   # создадим метод для отображения страницы about.html
   def hello
-    render file: "public/about.html"
+    #render file: "public/about.html"
+    render file: "#{Rails.root}/public/about.html", layout: false
   end
 end
