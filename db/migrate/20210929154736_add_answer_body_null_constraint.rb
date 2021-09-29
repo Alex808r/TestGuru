3e2f88018ed3ecfc824abbaccaf9b49f6bd8878a
Alex808r <fromxlam@gmail.com>
2021-09-29 18:50:18 +0300

    Add model Answer and table answers with constraint null to body

---
+++ db/migrate/20210929154736_add_answer_body_null_constraint.rb
@@ -0,0 +1,5 @@
+class AddAnswerBodyNullConstraint < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null(:answers, :body, false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_153448) do
+ActiveRecord::Schema.define(version: 2021_09_29_154736) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
