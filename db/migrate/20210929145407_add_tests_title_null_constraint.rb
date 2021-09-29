3479832db6e62587b37cb059869620bfce41bd04
Alex808r <fromxlam@gmail.com>
2021-09-29 19:18:01 +0300

    remove null constraint to level

--- db/migrate/20210929145407_add_tests_title_null_constraint.rb
+++ db/migrate/20210929145407_add_tests_title_null_constraint.rb
@@ -1,7 +1,6 @@
 class AddTestsTitleNullConstraint < ActiveRecord::Migration[6.1]
   def change
     change_column_null(:tests, :title, false)
-    change_column_null(:tests, :level, false)
   end
 
   # def up
