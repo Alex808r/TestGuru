0d1422561ab9113df65e7d1189c6376db7889a2a
Alex808r <fromxlam@gmail.com>
2021-09-29 19:31:09 +0300

    Add default value = 0 to table tests column level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_155918) do
+ActiveRecord::Schema.define(version: 2021_09_29_162802) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_155918) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
