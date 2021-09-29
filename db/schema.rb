9cced9a545cc91959d1fae7f772fa8ecce221e04
Alex808r <fromxlam@gmail.com>
2021-09-29 19:01:06 +0300

    Add model Category and table categories with null constraint to title

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_155315) do
+ActiveRecord::Schema.define(version: 2021_09_29_155918) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
