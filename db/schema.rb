75b1df577158777ad43fdeb7bb86e0ffbeb21d62
Alex808r <fromxlam@gmail.com>
2021-09-29 18:33:29 +0300

    Add table questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_145407) do
+ActiveRecord::Schema.define(version: 2021_09_29_153108) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
