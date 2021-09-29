28c8c0573088f35e0fbd65305fa3f8e1b857f1f5
Alex808r <fromxlam@gmail.com>
2021-09-29 19:07:23 +0300

    Add tests null constraint level(was rollback all migrations)

--- db/migrate/20210929145407_add_tests_title_null_constraint.rb
+++ db/migrate/20210929145407_add_tests_title_null_constraint.rb
@@ -1,6 +1,7 @@
 class AddTestsTitleNullConstraint < ActiveRecord::Migration[6.1]
   def change
     change_column_null(:tests, :title, false)
+    change_column_null(:tests, :level, false)
   end
 
   # def up
