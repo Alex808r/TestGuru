f1542a92e82830525084891572aad4fdf994ae63
Alex808r <fromxlam@gmail.com>
2021-09-29 18:56:09 +0300

    Add constraint null to users name and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_155111) do
+ActiveRecord::Schema.define(version: 2021_09_29_155315) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2021_09_29_155111) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
