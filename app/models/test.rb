d6502a06aa136eba1a377b35b0e52d77923a4b7c
Alex808r <fromxlam@gmail.com>
2021-10-02 15:01:20 +0300

    Add second variant method show_tests_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,11 @@ class Test < ApplicationRecord
       .where('categories.title = :category', category: category).order(id: :desc).pluck('tests.title')
   end
 
+  # работает только если указано отношение belongs_to :category
+  # def self.show_tests_by_category(category)
+  #   joins(:category).where('categories.title = :cat', cat: category).order(id: :desc).pluck('tests.title')
+  # end
+
 #    Test.joins(‘JOIN categories ON tests.category_id = categories.id’)
 
 =begin
