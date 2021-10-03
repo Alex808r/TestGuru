7cabab44142ff9ce8a57e90dd4115e4d04a7fac0
Alex808r <fromxlam@gmail.com>
2021-10-03 12:34:58 +0300

    Add bang to method create

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 
-users = User.create([
+users = User.create!([
   {name: "It is Author", email: "author@author.com"},
   {name: "Mike", email: "mike@mike.com"},
   {name: "Dave", email: "dave@dave.com"},
                     ])
 
-categories = Category.create([
+categories = Category.create!([
   {title: "Frontend"},
   {title: "Backend"},
   {title: "Data Science"},
   {title: "Mobile development"}
 ])
 
-tests =  Test.create([
+tests =  Test.create!([
   {title: "Ruby", category_id: categories[1].id, author_id: users[0].id},
   {title: "Ruby", level: 1, category_id: categories[1].id, author_id: users[0].id},
   {title: "Rails", category_id: categories[1].id, author_id: users[0].id},
@@ -30,7 +30,7 @@ tests =  Test.create([
   {title: "Swift", level: 2, category_id: Crategory.where(title: "Mobile development"), author_id: users[0].id}
 ])
 
-questions = Question.create([
+questions = Question.create!([
   {body: "Question 1", test_id: tests[0].id},
   {body: "Question 2", test_id: tests[0].id},
   {body: "Question 3", test_id: tests[1].id},
@@ -38,7 +38,7 @@ questions = Question.create([
   {body: "Question 5", test_id: tests[4].id}
 ])
 
-answers = Answer.create([
+answers = Answer.create!([
   {body: "Answer 1", question_id: questions[0].id},
   {body: "Answer 2", question_id: questions[0].id},
   {body: "Answer 1", question_id: questions[1].id},
