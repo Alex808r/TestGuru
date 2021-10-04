24c0c94a62d77a86eab23bd83c5a4f73d9ff5d08
Alex808r <fromxlam@gmail.com>
2021-10-04 15:42:09 +0300

    Addassociations to model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
 
+  has_many :tests_user
+  has_many :question
+  has_one :category
+
   def self.show_tests_by_category(category)
     joins('INNER JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category }).order(id: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tests_user
+
 
   def show_tests_by_level(level)
     Test.joins('INNER JOIN tests_users ON tests_users.test_id = tests.id')
