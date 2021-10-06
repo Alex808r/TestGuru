909c49d66fdd4249fd074a6ec38f98711bf1a222
Alex808r <fromxlam@gmail.com>
2021-10-06 15:10:40 +0300

    Delete exampels scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,21 +10,12 @@ class Test < ApplicationRecord
   scope :level_easy, -> {where(level: 0..1)}
   scope :level_medium, -> {where(level: 2..4)}
   scope :level_hard, -> {where(level: 5..Float::INFINITY)}
-
-# default_scope {order(created_at: :desc)}
-
-  #scope :easy, -> {where(level: 0..1).order(created_at: :desc)}
-  #scope :easy, ->(level) {where(level: level).order(created_at: :desc)}
-
-  # def self.easy(levl)
-  #   where(level: levl)
-  # end
+  scope :show_tests_by_category, -> (category) { joins(:category).where(categories: { title: category }) }
 
   # def self.show_tests_by_category(category)
   #   joins(:category).where(categories: { title: category }).order(id: :desc).pluck('tests.title')
   # end
 
-  scope :show_tests_by_category, -> (category) { joins(:category).where(categories: { title: category }) }
 
 
 end
