2cdbabaedf21232a6b779ae5bf11c66873ac8861
Alex808r <fromxlam@gmail.com>
2021-10-11 18:07:57 +0300

    Add method destroy and rescue_with

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,9 @@
 class QuestionsController < ApplicationController
 
-  before_action :set_test!, only: %i[index show create]
+  before_action :set_test!, only: %i[index show create destroy]
+  before_action :set_test!, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     # / tests/:test_id/questions(.:format)
@@ -28,14 +31,31 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    #/tests/:test_id/questions/:id
+    if @question.destroy
+      redirect_to test_questions_path
+    else
+      render plain: "The question was not deleted"
+    end
+  end
+
   private
 
   def set_test!
     @test = Test.find(params[:test_id])
   end
 
+  def set_question!
+    @question = @test.questions.find(params[:id])
+  end
+
   def question_params
     params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question was not found"
   end
 
 end
