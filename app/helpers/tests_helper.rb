25851320932af4e12dab8fb8dfd6f87ed9c5ca62
Alex808r <fromxlam@gmail.com>
2021-10-15 10:08:59 +0300

    First variant helper to test

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,12 @@
 module TestsHelper
+  def test_level(test)
+    case test.level
+    when 0 then 'easy'
+    when 1 then 'elementary'
+    when 2 then 'advanced'
+    when 3 then 'hard'
+    else
+      'hero'
+    end
+  end
 end
