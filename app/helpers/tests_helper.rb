eb118640d514182e10189c4b5514fedb641a9b2b
Alex808r <fromxlam@gmail.com>
2021-10-15 10:15:25 +0300

    Second variant helper to test

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,12 +1,18 @@
 module TestsHelper
+  TEST_LEVELS = {0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+
   def test_level(test)
-    case test.level
-    when 0 then 'easy'
-    when 1 then 'elementary'
-    when 2 then 'advanced'
-    when 3 then 'hard'
-    else
-      'hero'
-    end
+    TEST_LEVELS[test.level] || :hero
   end
+
+  # def test_level(test)
+  #   case test.level
+  #   when 0 then 'easy'
+  #   when 1 then 'elementary'
+  #   when 2 then 'advanced'
+  #   when 3 then 'hard'
+  #   else
+  #     'hero'
+  #   end
+  # end
 end
