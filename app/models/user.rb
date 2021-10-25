1026c374c47cb35a38d0009def08aa9c35c29849
Alex808r <fromxlam@gmail.com>
2021-10-25 11:09:58 +0300

    Move the authorization code to the module Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,38 +12,12 @@ class User < ApplicationRecord
 
   before_save :before_save_email_downcase
 
-  validates :name,  presence: true, length: { minimum: 3, maximum: 50 }
-  validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, if: Proc.new {|user| user.password_digest.blank?}
-  validates :password, confirmation: true
-
   def show_tests_by_level(level)
     tests.where(level: level)
   end
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  # Автотизация без has_secure_password
-  attr_reader :password
-  attr_writer :password_confirmation
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
   end
 
 
--- app/models/user/auth.rb
+++ app/models/user/auth.rb
@@ -1,3 +1,36 @@
 module User::Auth
+  extend ActiveSupport::Concern # для доступа к методам validates
 
+  # Автотизация без has_secure_password
+  attr_reader :password
+  attr_writer :password_confirmation
+
+  # подключаем методы validates в модуль
+  included do
+    validates :name,  presence: true, length: { minimum: 3, maximum: 50 }
+    validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, if: Proc.new {|user| user.password_digest.blank?}
+    validates :password, confirmation: true
+
+  end
+
+
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
+    end
+  end
+
+  private
+
+    def digest(string)
+      Digest::SHA1.hexdigest(string)
+    end
 end
