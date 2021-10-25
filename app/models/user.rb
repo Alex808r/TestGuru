c2643b974b5ca8cde4708de743ee53ffdc1d124f
Alex808r <fromxlam@gmail.com>
2021-10-25 11:44:49 +0300

    Change module Auth to method has_secure_password. Module Auth delete.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,24 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy, inverse_of: :author
 
   before_save :before_save_email_downcase
 
+  validates :name,  presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, if: Proc.new {|user| user.password_digest.blank?}
+  validates :password, confirmation: true
+
+
+  has_secure_password
+
   def show_tests_by_level(level)
     tests.where(level: level)
   end
+
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
