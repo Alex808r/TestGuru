44fc3e3886d1381cc3fc0f14ccd3894aaced1bcb
Alex808r <fromxlam@gmail.com>
2021-10-26 10:42:33 +0300

    Add dynamic title(provide and helper full title)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to author, "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener'
   end
+
+  def full_title(page_title = "")
+    base_title = "Test Guru"
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+
+  end
 end
