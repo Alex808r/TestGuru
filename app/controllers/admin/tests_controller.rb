ca7e88b9e2ea6c221f8cbdcf21c4c775f0b7650c
Alex808r <fromxlam@gmail.com>
2021-11-01 20:19:08 +0300

    Add all exampels localizations

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -22,7 +22,9 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.author_tests.new(test_params)
     if @test.save
+      flash[:notice] = t('.success')
       redirect_to admin_test_path(@test)
+      # redirect_to [:admin, @test]
     else
       render :new
     end
