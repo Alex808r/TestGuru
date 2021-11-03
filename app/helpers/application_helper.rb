e874ebc75fe8920764b53facc5fd9516df82e5eb
Alex808r <fromxlam@gmail.com>
2021-11-03 18:37:44 +0300

    Add second variant to scc class to flash message with helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,20 @@ module ApplicationHelper
       base_title
     end
   end
+
+  def class_for_flash_message(key)
+    case key
+    when 'success'
+      'alert-success'
+    when 'error'
+      'alert-danger'
+    when 'alert'
+      'alert-warning'
+    when 'notice'
+      'alert-info'
+    else
+      flash_type.to_s
+    end
+  end
+
 end
