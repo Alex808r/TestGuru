5004f3c5cafce33262cf0f90e26a7626de76d0fe
Alex808r <fromxlam@gmail.com>
2021-11-05 15:42:13 +0300

    Refactor method class_for_flash_message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,19 +18,27 @@ module ApplicationHelper
     end
   end
 
+  # def class_for_flash_message(key)
+  #   case key
+  #   when 'success' then 'alert-success'
+  #   when 'error' then 'alert-danger'
+  #   when 'alert' then 'alert-warning'
+  #   when 'notice' then 'alert-info'
+  #   else
+  #     key.to_s
+  #   end
+  # end
+
+  # использование таблицного метода
+  FLASH_TYPES_TO_CSS_CLASS = {
+    notice:  'info',
+    alert:   'warning',
+    error:   'danger'
+  }
+
   def class_for_flash_message(key)
-    case key
-    when 'success'
-      'alert-success'
-    when 'error'
-      'alert-danger'
-    when 'alert'
-      'alert-warning'
-    when 'notice'
-      'alert-info'
-    else
-      key.to_s
-    end
+    # (FLASH_TYPES_TO_CSS_CLASS[key.to_sym]).nil? ? key : FLASH_TYPES_TO_CSS_CLASS[key.to_sym]
+    FLASH_TYPES_TO_CSS_CLASS.fetch(key.to_sym, key)
   end
 
 end
