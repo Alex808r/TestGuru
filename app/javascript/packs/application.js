04e7edced6eed1f69363bd48cd8ed6076292edf5
Alex808r <fromxlam@gmail.com>
2021-11-09 14:59:39 +0300

    Add JS utilities

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "utilities/sorting"
+import "utilities/password_check"
 
 Rails.start()
 Turbolinks.start()
