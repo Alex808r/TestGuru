957c7c0e39e2068a7f4cb15918c3dc74a34c77e1
Alex808r <fromxlam@gmail.com>
2021-11-10 23:34:00 +0300

    Defaul DB postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_07_120348) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: true, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,44 +10,51 @@
 
 return if User.exists? && Test.exists? && Category.exists?
 
-users = User.create!([
-                       { name: 'It is Author', email: 'author@author.com' },
-                       { name: 'Mike', email: 'mike@mike.com' },
-                       { name: 'Dave', email: 'dave@dave.com' }
-                     ])
+
+users = User.create([
+                     {first_name: 'Main', last_name: 'Admin', name: 'Main Admin',
+                      email: 'admin@admin.com', password: '123456', type: 'Admin'}])
 
 categories = Category.create!([
                                 { title: 'Backend' },
-                                { title: 'Frontend' },
-                                { title: 'Data Science' },
-                                { title: 'Mobile development' }
+                                { title: 'Frontend' }
                               ])
 
 tests = Test.create([
-                      { title: 'Ruby 0', level: 0, category: categories[0], author: users[0] },
-                      { title: 'Ruby 1',   level: 1, category: categories[0], author: users[0] },
-                      { title: 'Rails 2',  level: 2, category: categories[0], author: users[0] },
-                      { title: 'Rails 3',  level: 3, category: categories[0], author: users[0] },
-                      { title: 'HTML 4',     level: 4, category: categories[1], author: users[0] },
-                      { title: 'Python 5',   level: 5, category: categories[2], author: users[0] },
-                      { title: 'Swift 6',    level: 6, category: categories[3], author: users[0] },
-                      { title: 'Swift 7',    level: 7, category: categories[3], author: users[0] }
+                      { title: 'Ruby 0',  level: 0, category: categories[0], author: users[0] },
+                      { title: 'Rails 2', level: 2, category: categories[0], author: users[0] },
+                      { title: 'HTML',    level: 1, category: categories[1], author: users[0] },
                     ])
 
 questions = Question.create!([
-                               { body: 'Question 1', test: tests[0] },
-                               { body: 'Question 2', test: tests[0] },
-                               { body: 'Question 3', test: tests[1] },
-                               { body: 'Question 4', test: tests[1] },
-                               { body: 'Question 5', test: tests[4] }
+                               { body: 'Who is the author of the Ruby language?', test: tests[0] },
+                               { body: 'Ruby typing', test: tests[0] },
+                               { body: 'What a local variable looks like', test: tests[0] },
+                               { body: 'Rails uses MVC?', test: tests[1] },
+                               { body: 'What is Ruby on Rails', test: tests[1] },
+                               { body: 'HTML protocol has state?', test: tests[2] },
+                               { body: 'What tag is used for links', test: tests[2] },
+
+
                              ])
 # rubocop:disable Lint/UselessAssignment
 answers = Answer.create!([
-                           { body: 'Answer 1', question: questions[0] },
-                           { body: 'Answer 2', question: questions[0] },
-                           { body: 'Answer 3', question: questions[2] },
-                           { body: 'Answer 3', question: questions[2] },
-                           { body: 'Answer 4', question: questions[3] },
-                           { body: 'Answer 5', question: questions[1] }
+                           { body: 'Matz', question: questions[0], correct: true },
+                           { body: 'Linus Torvalds', question: questions[0], correct: false },
+                           { body: 'Dynamic', question: questions[1], correct: true },
+                           { body: 'Static', question: questions[1], correct: false },
+                           { body: 'foo', question: questions[2],correct: true },
+                           { body: '@bar', question: questions[2],correct: false },
+                           { body: '@@bazz', question: questions[2],correct: false },
+                           { body: 'Yes', question: questions[3],correct: true },
+                           { body: 'No', question: questions[3],correct: false },
+                           { body: 'Framework', question: questions[4],correct: true },
+                           { body: 'Programing language', question: questions[4],correct: false },
+                           { body: 'Operating system', question: questions[4],correct: false },
+                           { body: 'Yes', question: questions[5],correct: false },
+                           { body: 'No', question: questions[5],correct: true },
+                           { body: '<a>', question: questions[6],correct: true },
+                           { body: '<b>', question: questions[6],correct: false },
+                           { body: '<div>', question: questions[6],correct: false }
                          ])
 # rubocop:enable Lint/UselessAssignment
