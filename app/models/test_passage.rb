d21ef591d05b4bf273ca11f6fa5773eaf4040a9a
Alex808r <fromxlam@gmail.com>
2021-11-11 01:36:23 +0300

    Add logic if the test has no questions

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -43,6 +43,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
+    return if self.current_question.answers.empty?
     # correct_answers.ids.sort == answer_ids&.map(&:to_i)&.sort
     correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
