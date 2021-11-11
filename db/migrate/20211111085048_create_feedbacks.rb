2f1ed00c100db0cbf35737afd5fcc9cfd9ee9055
Alex808r <fromxlam@gmail.com>
2021-11-11 11:56:03 +0300

    Add model Feedback

---
+++ db/migrate/20211111085048_create_feedbacks.rb
@@ -0,0 +1,12 @@
+class CreateFeedbacks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :feedbacks do |t|
+      t.string :user_name, null: false
+      t.string :email, null: false
+      t.string :header, null: false
+      t.string :content,null: false
+
+      t.timestamps
+    end
+  end
+end
