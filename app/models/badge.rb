a93f8811f620302ca068075be26cf9c3de03ed0d
Alex808r <fromxlam@gmail.com>
2021-11-12 18:12:43 +0300

    Add validate to model Badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,6 @@
 class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
+
+  validates :title, :image_url, :rule, :parameter, presence: true
 end
