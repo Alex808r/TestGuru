b1a8310a057d95ba817dc340723e3ee56f851550
Alex808r <fromxlam@gmail.com>
2021-11-12 15:58:04 +0300

    Add BadgesController to Admin

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,5 +1,50 @@
 # frozen_string_literal: true
 
 class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: %i[show edit update destroy]
 
+  def index
+    @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to [:admin, @badge], notice: "Бейдж успешно создан"
+      # redirect_to admin_badge_path(@badge), notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badge_path(@badge), notice: "Бейдж успешно обновлен"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :image_url, :rule, :parameter)
+  end
 end
