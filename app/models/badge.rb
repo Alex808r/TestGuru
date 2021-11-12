bd43beb182e55ffa79be7b1c47a5179945dccecc
Alex808r <fromxlam@gmail.com>
2021-11-12 18:57:58 +0300

    Add column success to table test passages

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,6 @@
 class Badge < ApplicationRecord
+  BADGES_RULES = %w[All_in_category On_first_try Certain_level].freeze
+
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_095936) do
+ActiveRecord::Schema.define(version: 2021_11_12_155614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_11_12_095936) do
     t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "success", default: false
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
