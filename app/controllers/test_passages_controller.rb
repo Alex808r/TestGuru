de001c5457c02bd93fc3da27f892866f0563a459
Alex808r <fromxlam@gmail.com>
2021-11-13 13:09:39 +0300

    Add receive the Badge

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -11,6 +11,19 @@ class TestPassagesController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
     if @test_passage.completed?
+
+      if @test_passage.test_successful?
+
+        @test_passage.success_true
+        badge_service = BadgeService.new(@test_passage)
+        badge_service.call
+        if badge_service.recieved
+          flash[:notice] = "#{t('.recieved')} #{view_context.link_to(t('.all_badges'), badges_path, target: '_blank').html_safe} "
+        end
+      end
+
+
+
       TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
     else
