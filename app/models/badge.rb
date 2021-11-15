bc4d44c7b00e44b5e1e5fa2330ce283bcee232d8
Alex808r <fromxlam@gmail.com>
2021-11-15 18:43:56 +0300

    enum rules from array to hash

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,11 @@
 
 class Badge < ApplicationRecord
 
-  enum rules: [:all_in_category?, :on_first_try?, :all_tests_by_level?]
+  enum rules: {
+    all_in_category?: "all_in_category",
+    on_first_try?: "on_first_try",
+    all_tests_by_level?: "all_tests_by_level"
+  }
 
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
