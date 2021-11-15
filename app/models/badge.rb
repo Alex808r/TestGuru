50419106f1ee9d800a274397fc047026f90e6212
Alex808r <fromxlam@gmail.com>
2021-11-15 08:48:45 +0300

    Add enum to model badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Badge < ApplicationRecord
-  BADGES_RULES = %w[all_in_category on_first_try all_tests_by_level].freeze
+  # BADGES_RULES = %w[all_in_category on_first_try all_tests_by_level].freeze
+
+  enum rules: [:all_in_category, :on_first_try, :all_tests_by_level]
 
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
